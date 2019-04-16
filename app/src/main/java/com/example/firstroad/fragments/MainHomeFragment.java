f3b7b514e732d1e31c38da9c77a8f903c12253b9
huangyuanhao <307975180@qq.com>
2019-04-16 21:38:18 +0800

    目的地基本布局结束

--- app/src/main/java/com/example/firstroad/fragments/MainHomeFragment.java
+++ app/src/main/java/com/example/firstroad/fragments/MainHomeFragment.java
@@ -10,21 +10,12 @@ import android.view.ViewGroup;
 
 import com.example.firstroad.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MainHomeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MainHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MainHomeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -34,15 +25,6 @@ public class MainHomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainHomeFragment newInstance(String param1, String param2) {
         MainHomeFragment fragment = new MainHomeFragment();
         Bundle args = new Bundle();
@@ -81,16 +63,6 @@ public class MainHomeFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
