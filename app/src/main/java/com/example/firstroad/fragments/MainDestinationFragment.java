a2547f55a6419fde116aa22a6c358bd846e4e2cf
huangyuanhao <307975180@qq.com>
2019-04-16 18:16:09 +0800

    目的地基本布局结束

--- app/src/main/java/com/example/firstroad/fragments/MainDestinationFragment.java
+++ app/src/main/java/com/example/firstroad/fragments/MainDestinationFragment.java
@@ -10,21 +10,11 @@ import android.view.ViewGroup;
 
 import com.example.firstroad.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MainDestinationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MainDestinationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainDestinationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -34,15 +24,6 @@ public class MainDestinationFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainDestinationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainDestinationFragment newInstance(String param1, String param2) {
         MainDestinationFragment fragment = new MainDestinationFragment();
         Bundle args = new Bundle();
@@ -68,7 +49,6 @@ public class MainDestinationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main_destination, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -81,18 +61,7 @@ public class MainDestinationFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/example/firstroad/fragments/MainMineFragment.java
+++ app/src/main/java/com/example/firstroad/fragments/MainMineFragment.java
@@ -10,14 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.firstroad.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MainMineFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MainMineFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainMineFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,15 +26,6 @@ public class MainMineFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainMineFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainMineFragment newInstance(String param1, String param2) {
         MainMineFragment fragment = new MainMineFragment();
         Bundle args = new Bundle();
@@ -81,16 +64,6 @@ public class MainMineFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
