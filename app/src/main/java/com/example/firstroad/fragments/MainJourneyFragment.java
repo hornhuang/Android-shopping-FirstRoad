0a0bfce779a57f98a3354d82f739037362d34b34
huangyuanhao <307975180@qq.com>
2019-04-18 15:07:05 +0800

    目的地基本布局结束

--- app/src/main/java/com/example/firstroad/fragments/MainJourneyFragment.java
+++ app/src/main/java/com/example/firstroad/fragments/MainJourneyFragment.java
@@ -10,14 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.firstroad.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MainJourneyFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MainJourneyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainJourneyFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,15 +26,6 @@ public class MainJourneyFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainJourneyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainJourneyFragment newInstance(String param1, String param2) {
         MainJourneyFragment fragment = new MainJourneyFragment();
         Bundle args = new Bundle();
@@ -68,7 +51,6 @@ public class MainJourneyFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main_journey, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -81,18 +63,7 @@ public class MainJourneyFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
